86bf7c902507917606586f428e78ca5a14b0dfb3
Marco De Falco <mrc.defalco90@gmail.com>
2021-11-02 19:06:51 +0100

    tests

--- Smart_contract_part/test/test_BlockgemsNFTCollection.js
+++ Smart_contract_part/test/test_BlockgemsNFTCollection.js
@@ -1,54 +1,50 @@
-const MetaCoin = artifacts.require("MetaCoin");
+const BlockgemsNFTcollection = artifacts.require("BlockgemsNFTCollection");
 
-contract("2nd MetaCoin test", async accounts => {
-  it("should put 10000 MetaCoin in the first account", async () => {
-    const instance = await MetaCoin.deployed();
-    const balance = await instance.getBalance.call(accounts[0]);
-    assert.equal(balance.valueOf(), 10000);
+contract("BlockgemsNFTCollection", async accounts => {
+  it("should put 1 NFT into the owner's wallet at launch", async () => {
+    const instance = await BlockgemsNFTcollection.deployed();
+    const balance = await instance.balanceOf(accounts[0]);
+    assert.equal(balance.valueOf(), 1);
+  });
+  it("should pause the minting", async () => {
+    const instance = await BlockgemsNFTcollection.deployed();
+    const new_state = await instance.pause(false);
+    assert.equal(new_state.receipt.status, true);
+  });
+  it("should unpause the minting", async () => {
+    const instance = await BlockgemsNFTcollection.deployed();
+    const new_state = await instance.pause(true);
+    assert.equal(new_state.receipt.status, true);
+  });
+  it("should get 'https://replaceme1.json' as a TokenURI for token #1 ", async () => {
+    const instance = await BlockgemsNFTcollection.deployed();
+    const base_uri = await instance.tokenURI(1);
+    assert.equal(base_uri.valueOf(), 'https://replaceme1.json');
+  });
+  it("should change 'https://replaceme' to 'https://newhosting' as a TokenURI for token #1 ", async () => {
+    const instance = await BlockgemsNFTcollection.deployed();
+    const new_base_uri = await instance.setBaseURI('https://newhosting');
+    assert.equal(new_base_uri.receipt.status, true);
   });
 
-  it("should call a function that depends on a linked library", async () => {
-    const meta = await MetaCoin.deployed();
-    const outCoinBalance = await meta.getBalance.call(accounts[0]);
-    const metaCoinBalance = outCoinBalance.toNumber();
-    const outCoinBalanceEth = await meta.getBalanceInEth.call(accounts[0]);
-    const metaCoinEthBalance = outCoinBalanceEth.toNumber();
-    assert.equal(metaCoinEthBalance, 2 * metaCoinBalance);
+
+  it("should get 'https://newhosting1.json' as a TokenURI for token #1 ", async () => {
+    const instance = await BlockgemsNFTcollection.deployed();
+    const base_uri = await instance.tokenURI(1);
+    assert.equal(base_uri.valueOf(), 'https://newhosting1.json');
   });
 
-  it("should send coin correctly", async () => {
-    // Get initial balances of first and second account.
-    const account_one = accounts[0];
-    const account_two = accounts[1];
-    let balance;
 
-    const amount = 10;
-
-    const instance = await MetaCoin.deployed();
-    const meta = instance;
-
-    balance = await meta.getBalance.call(account_one);
-    const account_one_starting_balance = balance.toNumber();
-
-    balance = await meta.getBalance.call(account_two);
-    const account_two_starting_balance = balance.toNumber();
-    await meta.sendCoin(account_two, amount, { from: account_one });
-
-    balance = await meta.getBalance.call(account_one);
-    const account_one_ending_balance = balance.toNumber();
-
-    balance = await meta.getBalance.call(account_two);
-    const account_two_ending_balance = balance.toNumber();
-
-    assert.equal(
-      account_one_ending_balance,
-      account_one_starting_balance - amount,
-      "Amount wasn't correctly taken from the sender"
-    );
-    assert.equal(
-      account_two_ending_balance,
-      account_two_starting_balance + amount,
-      "Amount wasn't correctly sent to the receiver"
-    );
+  it("should whitelist account 2 ", async () => {
+    const instance = await BlockgemsNFTcollection.deployed();
+    const new_wl_account = await instance.whitelistUser(accounts[1]);
+    assert.equal(new_wl_account.receipt.status, true);;
   });
-});
+
+  it("should confirm that account 2 is on the whitelist ", async () => {
+    const instance = await BlockgemsNFTcollection.deployed();
+    const wl = await instance.whitelisted(accounts[1]);
+    assert.equal(wl, true);
+  });
+
+})
